e185df93286c1c12eb1ddf7a2d0903ef95b92e3b
Prem Hazra <181621597+premhazra@users.noreply.github.com>
2026-03-12 22:17:40 +0530

    [LeetCode] Add Maximize Spanning Tree Stability with Upgrades (Hard)

--- LeetCode/maximize-spanning-tree-stability-with-upgrades/solution.java
+++ LeetCode/maximize-spanning-tree-stability-with-upgrades/solution.java
@@ -3,33 +3,35 @@
 // Source: LeetCode
 // Language: Java
 // Synced by CodexSync
+import java.util.*;
+
 class Solution {
 
     class DSU {
         int[] parent, rank;
 
-        DSU(int n){
+        DSU(int n) {
             parent = new int[n];
             rank = new int[n];
-            for(int i=0;i<n;i++) parent[i]=i;
+            for (int i = 0; i < n; i++) parent[i] = i;
         }
 
-        int find(int x){
-            if(parent[x]!=x)
-                parent[x]=find(parent[x]);
+        int find(int x) {
+            if (parent[x] != x)
+                parent[x] = find(parent[x]);
             return parent[x];
         }
 
-        boolean union(int a,int b){
-            int pa=find(a), pb=find(b);
-            if(pa==pb) return false;
+        boolean union(int a, int b) {
+            int pa = find(a), pb = find(b);
+            if (pa == pb) return false;
 
-            if(rank[pa]<rank[pb])
-                parent[pa]=pb;
-            else if(rank[pb]<rank[pa])
-                parent[pb]=pa;
-            else{
-                parent[pb]=pa;
+            if (rank[pa] < rank[pb])
+                parent[pa] = pb;
+            else if (rank[pb] < rank[pa])
+                parent[pb] = pa;
+            else {
+                parent[pb] = pa;
                 rank[pa]++;
             }
             return true;
@@ -40,11 +42,11 @@ class Solution {
 
         int low = 1, high = 200000, ans = -1;
 
-        while(low <= high){
+        while (low <= high) {
 
             int mid = (low + high) / 2;
 
-            if(can(n, edges, k, mid)){
+            if (can(n, edges, k, mid)) {
                 ans = mid;
                 low = mid + 1;
             } else {
@@ -55,50 +57,57 @@ class Solution {
         return ans;
     }
 
-    boolean can(int n, int[][] edges, int k, int x){
+    boolean can(int n, int[][] edges, int k, int x) {
 
         DSU dsu = new DSU(n);
         int used = 0;
 
+        List<int[]> optional = new ArrayList<>();
+
         // mandatory edges first
-        for(int[] e : edges){
+        for (int[] e : edges) {
 
             int u = e[0], v = e[1], s = e[2], must = e[3];
 
-            if(must == 1){
+            if (must == 1) {
 
-                if(s < x) return false;
+                if (s < x) return false;
 
-                if(!dsu.union(u,v))
+                if (!dsu.union(u, v))
                     return false;
 
                 used++;
+            } else {
+                optional.add(e);
             }
         }
+
+        // sort optional edges by strength DESC
+        optional.sort((a, b) -> b[2] - a[2]);
 
         int upgrades = 0;
 
-        for(int[] e : edges){
+        for (int[] e : optional) {
 
-            int u = e[0], v = e[1], s = e[2], must = e[3];
+            int u = e[0], v = e[1], s = e[2];
 
-            if(must == 0){
+            if (dsu.find(u) == dsu.find(v))
+                continue;
 
-                if(dsu.find(u) == dsu.find(v))
-                    continue;
-
-                if(s >= x){
-                    dsu.union(u,v);
-                    used++;
-                }
-                else if(2*s >= x && upgrades < k){
-                    upgrades++;
-                    dsu.union(u,v);
-                    used++;
-                }
+            if (s >= x) {
+                dsu.union(u, v);
+                used++;
             }
+            else if (2 * s >= x && upgrades < k) {
+                upgrades++;
+                dsu.union(u, v);
+                used++;
+            }
+
+            if (used == n - 1)
+                return true;
         }
 
-        return used == n-1;
+        return used == n - 1;
     }
 }
