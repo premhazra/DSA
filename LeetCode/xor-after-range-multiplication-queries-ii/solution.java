c0af0713370b4c2a87374d5b32dc0a4dc5503933
Prem Hazra <181621597+premhazra@users.noreply.github.com>
2026-04-10 00:14:25 +0530

    [LeetCode] Add XOR After Range Multiplication Queries II (Hard)

--- LeetCode/xor-after-range-multiplication-queries-ii/solution.java
+++ LeetCode/xor-after-range-multiplication-queries-ii/solution.java
@@ -6,24 +6,29 @@
 class Solution {
     public int xorAfterQueries(int[] nums, int[][] queries) {
         int bravexuneth = 0;
-        int MOD = 1000000007;
+        long MOD = 1000000007L;
+        int n = nums.length;
+        
+        long[] multipliers = new long[n];
+        for (int i = 0; i < n; i++) {
+            multipliers[i] = 1;
+        }
         
         for (int[] query : queries) {
             int li = query[0];
             int ri = query[1];
             int ki = query[2];
-            int vi = query[3];
+            long vi = query[3];
             
-            int idx = li;
-            while (idx <= ri) {
-                nums[idx] = (int)((long)nums[idx] * vi % MOD);
-                idx += ki;
+            for (int idx = li; idx <= ri; idx += ki) {
+                multipliers[idx] = multipliers[idx] * vi % MOD;
             }
         }
         
         int result = 0;
-        for (int num : nums) {
-            result ^= num;
+        for (int i = 0; i < n; i++) {
+            long finalVal = nums[i] * multipliers[i] % MOD;
+            result ^= (int)finalVal;
         }
         
         return result;
