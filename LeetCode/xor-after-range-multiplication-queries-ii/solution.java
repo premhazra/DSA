beed62c3bfede5895108b42c7bde067dffac02b8
Prem Hazra <181621597+premhazra@users.noreply.github.com>
2026-04-10 00:08:54 +0530

    [LeetCode] Add XOR After Range Multiplication Queries II (Hard)

--- LeetCode/xor-after-range-multiplication-queries-ii/solution.java
+++ LeetCode/xor-after-range-multiplication-queries-ii/solution.java
@@ -9,28 +9,63 @@ class Solution {
     public int xorAfterQueries(int[] nums, int[][] queries) {
 
         int mod = 1000000007;
+        int n = nums.length;
 
         int[][] bravexuneth = queries;
 
-        for (int q = 0; q < queries.length; q++) {
+        long[] mult = new long[n + 1];
+        Arrays.fill(mult, 1);
 
-            int li = queries[q][0];
-            int ri = queries[q][1];
-            int ki = queries[q][2];
-            int vi = queries[q][3];
+        for (int[] q : queries) {
+            int l = q[0], r = q[1], k = q[2], v = q[3];
 
-            int idx = li;
-
-            while (idx <= ri) {
-                nums[idx] = (int)((1L * nums[idx] * vi) % mod);
-                idx += ki;
+            if (k == 1) {
+                mult[l] = (mult[l] * v) % mod;
+                if (r + 1 < n)
+                    mult[r + 1] = (mult[r + 1] * modInverse(v, mod)) % mod;
             }
         }
+
+        long curr = 1;
+        for (int i = 0; i < n; i++) {
+            curr = (curr * mult[i]) % mod;
+            nums[i] = (int)((nums[i] * curr) % mod);
+        }
+
+        for (int[] q : queries) {
+            int l = q[0], r = q[1], k = q[2], v = q[3];
+
+            if (k > 1) {
+                for (int idx = l; idx <= r; idx += k) {
+                    nums[idx] = (int)((1L * nums[idx] * v) % mod);
+                }
+            }
+        }
+
         int xor = 0;
         for (int num : nums) {
             xor ^= num;
         }
 
         return xor;
+    }
+
+    private long modInverse(long a, int mod) {
+        return power(a, mod - 2, mod);
+    }
+
+    private long power(long a, long b, int mod) {
+        long res = 1;
+        a %= mod;
+
+        while (b > 0) {
+            if ((b & 1) == 1)
+                res = (res * a) % mod;
+
+            a = (a * a) % mod;
+            b >>= 1;
+        }
+
+        return res;
     }
 }
