40218a22805a90b7659aa929243e2b83218c7dfa
Prem Hazra <181621597+premhazra@users.noreply.github.com>
2026-06-08 03:30:21 +0530

    [LeetCode] Add Plus One (Easy)

---
+++ LeetCode/plus-one/solution.java
@@ -0,0 +1,22 @@
+// Problem: Plus One
+// Difficulty: Easy
+// Source: LeetCode
+// Language: Java
+// Synced by CodexSync
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        // All digits were 9
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res;
+    }
+}
